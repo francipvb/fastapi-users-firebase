5a0e3b299d98dc7d41e993fd42d887b8ef823d0a
Francisco R. Del Roio <francipvb@hotmail.com>
2024-02-20 03:52:17 -0300

    test: added some tests for the database and user objects

--- tests/test_user_store.py
+++ tests/test_user_store.py
@@ -1,22 +1,37 @@
-from unittest.mock import Mock, sentinel
+from unittest.mock import Mock, create_autospec, sentinel
 
+import firebase_admin
 import pytest
 from faker import Faker
 from firebase_admin import auth
 
 from fastapi_users_firebase import FirebaseUserDatabase
+from fastapi_users_firebase.user_store import FirebaseUser
 
 
 @pytest.fixture()
-def database():
+def firebase_app() -> firebase_admin.App:
+    """Build a mocked firebase app object.
+
+    Returns:
+        A firebase app mock.
+    """
+    return create_autospec(firebase_admin.App)
+
+
+@pytest.fixture()
+def database(firebase_app: firebase_admin.App):
     """Builds a firebase user database.
 
     This is a fixture function to build a test object.
 
+    Args:
+        firebase_app: The app to assign to the database object
+
     Returns:
         An user database that interacts with Firebase.
     """
-    return FirebaseUserDatabase()
+    return FirebaseUserDatabase(firebase_app)
 
 
 class TestFirebaseUserDatabase:
@@ -31,6 +46,12 @@ class TestFirebaseUserDatabase:
         mock_obj = Mock()
         monkeypatch.setattr(auth, auth.get_user_by_email.__name__, mock_obj)
         return mock_obj
+
+    @pytest.fixture()
+    def delete_user_mock(self, monkeypatch: pytest.MonkeyPatch) -> Mock:
+        mock = Mock()
+        monkeypatch.setattr(auth, auth.delete_user.__name__, mock)
+        return mock
 
     @pytest.mark.anyio()
     async def test_get(self, faker: Faker, get_user_mock: Mock, database: FirebaseUserDatabase) -> None:
@@ -69,3 +90,54 @@ class TestFirebaseUserDatabase:
         result = await database.get_by_email(email)
         assert result is None
         get_user_by_email_mock.assert_called_with(email, database._app)
+
+    @pytest.mark.anyio()
+    async def test_delete_invalid(self, database: FirebaseUserDatabase, delete_user_mock: Mock) -> None:
+        with pytest.raises(TypeError):
+            await database.delete(sentinel)
+
+        delete_user_mock.assert_not_called()
+
+    @pytest.mark.anyio()
+    async def test_delete_different_apps(self, database: FirebaseUserDatabase, delete_user_mock: Mock) -> None:
+        user = FirebaseUser(create_autospec(auth.UserRecord), create_autospec(firebase_admin.App))
+        with pytest.raises(AssertionError):
+            await database.delete(user)
+
+        delete_user_mock.assert_not_called()
+
+    @pytest.mark.anyio()
+    async def testdelete(
+        self, database: FirebaseUserDatabase, delete_user_mock: Mock, firebase_app: firebase_admin.App
+    ) -> None:
+        record = create_autospec(auth.UserRecord)
+        user = FirebaseUser(record, firebase_app)
+        await database.delete(user)
+        delete_user_mock.assert_called_once_with(user.id)
+
+
+class TestFirebaseUser:
+    @pytest.fixture
+    def user_spec(self) -> Mock:
+        return create_autospec(auth.UserRecord)
+
+    @pytest.fixture()
+    def user(self, user_spec: Mock) -> FirebaseUser:
+        return FirebaseUser(user_spec)
+
+    def test_id(self, user: FirebaseUser, user_spec: Mock, faker: Faker) -> None:
+        user_spec.uid = faker.pystr()
+        assert user.id == user_spec.uid
+
+    def test_email(self, user: FirebaseUser, user_spec: Mock):
+        assert user.email is user_spec.email
+
+    def test_is_verified_with_email(self, user: FirebaseUser, user_spec: Mock) -> None:
+        user_spec.email_verified = True
+        user_spec.phone_number = None
+        assert user.is_verified
+
+    def test_is_verified_with_phone(self, user: FirebaseUser, user_spec: Mock, faker: Faker) -> None:
+        user_spec.phone_number = faker.phone_number()
+        user_spec.email_verified = False
+        assert user.is_verified
