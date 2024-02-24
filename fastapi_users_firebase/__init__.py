24b009a148bca8cd40e432cde91edc140f4d8c77
Francisco R. Del Roio <francipvb@hotmail.com>
2024-02-24 11:50:34 -0300

    fixed exports.

--- fastapi_users_firebase/__init__.py
+++ fastapi_users_firebase/__init__.py
@@ -3,6 +3,14 @@
 This is the entrypoint of a plugin to interact with Firebase Authentication from FastAPI users plugin.
 """
 
+from .schemas import CreateFirebaseUserModel, CreateUpdateFirebaseUserModel, UpdateFirebaseUserModel
+from .user import FirebaseUser
 from .user_database import FirebaseUserDatabase
 
-__all__ = ["user", "FirebaseUserDatabase"]
+__all__ = [
+    "FirebaseUser",
+    "FirebaseUserDatabase",
+    "CreateFirebaseUserModel",
+    "CreateUpdateFirebaseUserModel",
+    "UpdateFirebaseUserModel",
+]
